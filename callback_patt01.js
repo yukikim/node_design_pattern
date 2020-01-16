6001f12a8093c3dd266ebf2c4e4f626887d6f6bf
yukikim <tki6ra@gmail.com>
2020-01-16 11:55:26 +0900

    コールバック関数の例

---
+++ callback_patt01.js
@@ -0,0 +1,19 @@
+/**
+ * コールバック関数の使用
+ */
+
+"use strict";
+
+/*
+ function add(a, b) {
+   return a + b;
+ }
+*/
+
+function add(a, b, callback) {
+  callback(a + b);
+}
+
+console.log('before');
+add(1, 2, result => console.log('Result: ' + result));
+console.log('after');
